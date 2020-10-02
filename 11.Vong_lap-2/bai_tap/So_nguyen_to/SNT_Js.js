9c308779fff2a7aebf13f5e0a344fe880c53492c
ledinhthinh <ldt051202@gmail.com>
2020-10-02 15:25:46 +0700

    up thuc hanh ham

--- 11.Vong_lap-2/bai_tap/So_nguyen_to/SNT_Js.js
+++ 11.Vong_lap-2/bai_tap/So_nguyen_to/SNT_Js.js
@@ -1,21 +1,21 @@
 let number;
-let temp =2;
-let count =0;
+let temp = 2;
+let count = 0;
 let check = false;
-do{
-    number =parseInt(prompt("nhap so luong so nguyen to can hien thi: "));
-}  while (isNaN(number));
-while (count <number) {
-    for (let i=2; i<=Math.sqrt(temp);i++) {
-        if(temp%i ===0) {
-            check =true;
+do {
+    number = parseInt(prompt("nhap so luong so nguyen to can hien thi: "));
+} while (isNaN(number));
+while (count < number) {
+    for (let i = 2; i <= Math.sqrt(temp); i++) {
+        if (temp % i === 0) {
+            check = true;
             break;
         }
     }
-    if(!check) {
-        document.write(temp+" <br>")
+    if (!check) {
+        document.write(temp + " <br>")
         count++;
     }
-    check =false;
+    check = false;
     temp++;
 }
