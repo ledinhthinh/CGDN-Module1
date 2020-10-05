6f91cfa3b7c168800f060cae05b9e0f77e0881a1
ledinhthinh <ldt051202@gmail.com>
2020-10-05 15:52:20 +0700

    up bai ham

--- CaseStudy/Module1_Lê_Đình_Thịnh/Furama.js
+++ CaseStudy/Module1_Lê_Đình_Thịnh/Furama.js
@@ -18,107 +18,128 @@ function display() {
         email.focus;
         return false;
     } else {
-        document.getElementById("displayEmail").innerText = email;
-    }
-//
-    document.getElementById("name").value = "";
-    document.getElementById("cmnd").value = "";
-    document.getElementById("date").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("address").value = "";
-    document.getElementById("customer").value = "";
-    document.getElementById("people").value = "";
-    document.getElementById("rent").value = "";
-    document.getElementById("service").value = "";
-    document.getElementById("room").value = "";
-    document.getElementById("code").value = "";
-    document.getElementById("displayName").innerText = name;
-    document.getElementById("displayCmnd").innerText = cmnd;
-    document.getElementById("displayDate").innerText = date;
-    document.getElementById("displayEmail").innerText = email;
-    document.getElementById("displayAddress").innerText = address;
-    document.getElementById("displayCustomer").innerText = customer;
-    document.getElementById("displayPeople").innerText = people;
-    document.getElementById("displayRent").innerText = rent;
-    document.getElementById("displayService").innerText = service;
-    document.getElementById("displayRoom").innerText = room;
-    //tính tổng tiền phải trả
-    //giảm giá theo địa chỉ
-    let saleAdd = 0;
-    switch (address) {
-        case "Đà Nẵng":
-            saleAdd = 20;
-            break;
-        case "Huế":
-            saleAdd = 10;
-            break;
-        case "Quảng Nam":
-            saleAdd = 5;
-            break;
-        default:
-            saleAdd = 0;
-    }
-//giảm giá theo số ngày
-    let saleRent = 0;
-    if (rent > 7) {
-        saleRent = 30;
-    } else if (rent > 5) {
-        saleRent = 20;
-    } else if (rent > 2) {
-        saleRent = 10;
-    } else {
-        saleRent = 0;
-    }
-//giảm giá theo loại khách hàng
-    let saleCus = 0;
-    switch (customer) {
-        case "Diamond":
-            saleCus = 15;
-            break;
-        case "Platinum":
-            saleCus = 10;
-            break;
-        case "Gold":
-            saleCus = 5;
-            break;
-        case "Silver":
-            saleCus = 2;
-            break;
-        case "Member":
-            saleCus = 0;
-            break;
-    }
-    let saleCode = 0;
-    switch (code) {
-        case "FURAMA":
-            saleCode = 20;
-            break;
-        case "DANANG":
-            saleCode = 10;
-            break;
-        case "SONGHAN":
-            saleCode = 5;
-            break;
-        default:
-            saleCode = 0;
-    }
-    let allsale = 0;
-    allsale = saleAdd + saleCus + saleRent + saleCode;
-    let outPay = 0;
-    switch (service) {
-        case "Villa" :
-            outPay = 500 * rent - allsale;
-            break;
-        case "House" :
-            outPay = 300 * rent - allsale;
-            break;
-        case "Room" :
-            outPay = 100 * rent - allsale;
-            break;
-    }
-    document.getElementById('displaySale').innerText = allsale;
-    document.getElementById('displayPay').innerText = outPay;
 
+//kt cmnd
+        let checkcmnd = Number.isInteger(parseInt(cmnd));
+        if ((checkcmnd === false) ) {
+            alert('Nhập sai số chứng minh,xin mời nhập lại!!!');
+        } else {
+//kt số người đi kèm và số ngày
+            let checkpeo = Number.isInteger(people);
+            if ((checkpeo === false) || (people < 0)) {
+                alert('Nhập sai số người đi kèm,xin mời nhập lại!!!');
+            } else {
+
+//kt số ngày
+                let checkrent = Number.isInteger(rent);
+                if ((checkrent === false) || (rent < 0)) {
+                    alert('Nhập sai số ngày,xin mời nhập lại!!!');
+                } else {
+//
+                    document.getElementById("name").value = "";
+                    document.getElementById("cmnd").value = "";
+                    document.getElementById("date").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("address").value = "";
+                    document.getElementById("customer").value = "";
+                    document.getElementById("people").value = "";
+                    document.getElementById("rent").value = "";
+                    document.getElementById("service").value = "";
+                    document.getElementById("room").value = "";
+                    document.getElementById("code").value = "";
+                    document.getElementById("displayName").innerText = name;
+                    document.getElementById("displayCmnd").innerText = cmnd;
+                    document.getElementById("displayDate").innerText = date;
+                    document.getElementById("displayEmail").innerText = email;
+                    document.getElementById("displayAddress").innerText = address;
+                    document.getElementById("displayCustomer").innerText = customer;
+                    document.getElementById("displayPeople").innerText = people;
+                    document.getElementById("displayRent").innerText = rent;
+                    document.getElementById("displayService").innerText = service;
+                    document.getElementById("displayRoom").innerText = room;
+
+
+                    //tính tổng tiền phải trả
+                    //giảm giá theo địa chỉ
+                    let saleAdd = 0;
+                    switch (address) {
+                        case "Đà Nẵng":
+                            saleAdd = 20;
+                            break;
+                        case "Huế":
+                            saleAdd = 10;
+                            break;
+                        case "Quảng Nam":
+                            saleAdd = 5;
+                            break;
+                        default:
+                            saleAdd = 0;
+                    }
+//giảm giá theo số ngày
+                    let saleRent = 0;
+                    if (rent > 7) {
+                        saleRent = 30;
+                    } else if (rent > 5) {
+                        saleRent = 20;
+                    } else if (rent > 2) {
+                        saleRent = 10;
+                    } else {
+                        saleRent = 0;
+                    }
+//giảm giá theo loại khách hàng
+                    let saleCus = 0;
+                    switch (customer) {
+                        case "Diamond":
+                            saleCus = 15;
+                            break;
+                        case "Platinum":
+                            saleCus = 10;
+                            break;
+                        case "Gold":
+                            saleCus = 5;
+                            break;
+                        case "Silver":
+                            saleCus = 2;
+                            break;
+                        case "Member":
+                            saleCus = 0;
+                            break;
+                    }
+                    let saleCode = 0;
+                    switch (code) {
+                        case "FURAMA":
+                            saleCode = 20;
+                            break;
+                        case "DANANG":
+                            saleCode = 10;
+                            break;
+                        case "SONGHAN":
+                            saleCode = 5;
+                            break;
+                        default:
+                            saleCode = 0;
+                    }
+                    let allsale = 0;
+                    allsale = saleAdd + saleCus + saleRent + saleCode;
+                    let outPay = 0;
+                    switch (service) {
+                        case "Villa" :
+                            outPay = 500 * rent - allsale;
+                            break;
+                        case "House" :
+                            outPay = 300 * rent - allsale;
+                            break;
+                        case "Room" :
+                            outPay = 100 * rent - allsale;
+                            break;
+                    }
+                    document.getElementById('displaySale').innerText = allsale;
+                    document.getElementById('displayPay').innerText = outPay;
+
+                }
+            }
+        }
+    }
 }
 
 
